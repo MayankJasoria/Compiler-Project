a0849aeb344dab52486e8722e3dc9b502ac28336
Mayank Jasoria <jasoriamayank@gmail.com>
2020-02-19 22:57:41 +0530

    Restructuring: moved definitions to utils.h

--- lexerDef.h
+++ lexerDef.h
@@ -1,74 +1,13 @@
 #ifndef _LEXERDEF
 #define _LEXERDEF
 
+#include "utils/utils.h"
+
 #define NUM_KEYWORDS 30
 
 #define HASH_TABLE_SIZE 199
 
 int num_keywords;
-
-/* enum for the tokens */
-/* TODO: Replace '#define'd tokens with the enum -- uncomment to use enum  */
-typedef enum {
-	EMPTY,
-	INTEGER,
-	REAL,
-	BOOLEAN,
-	OF,
-	ARRAY,
-	START,
-	END,
-	DECLARE,
-	MODULE,
-	DRIVER,
-	PROGRAM,
-	GET_VALUE,
-	PRINT,
-	USE,
-	WITH,
-	PARAMETERS,
-	TRUE,
-	FALSE,
-	TAKES,
-	INPUT,
-	RETURNS,
-	AND,
-	OR,
-	FOR,
-	IN,
-	SWITCH,
-	CASE,
-	BREAK,
-	DEFAULT,
-	WHILE,
-	PLUS,
-	MINUS,
-	MUL,
-	DIV,
-	LT,
-	LE,
-	GE,
-	GT,
-	EQ,
-	NE,
-	DEF,
-	ENDDEF,
-	COLON,
-	RANGEOP,
-	SEMICOL,
-	COMMA,
-	ASSIGNOP,
-	SQBO,
-	SQBC,
-	BO,
-	BC,
-	NUM,
-	RNUM,
-	ID,
-	DRIVERDEF,
-	DRIVERENDDEF,
-	DOLLAR
-} terminal;
 
 /* type definition of a Token */
 typedef struct {
--- utils/utils.h
+++ utils/utils.h
@@ -12,4 +12,132 @@ typedef enum {
     FALSE, TRUE
 } boolean;
 
+/* enum for the tokens */
+typedef enum {
+	EMPTY,
+	INTEGER,
+	REAL,
+	BOOLEAN,
+	OF,
+	ARRAY,
+	START,
+	END,
+	DECLARE,
+	MODULE,
+	DRIVER,
+	PROGRAM,
+	GET_VALUE,
+	PRINT,
+	USE,
+	WITH,
+	PARAMETERS,
+	TRUE,
+	FALSE,
+	TAKES,
+	INPUT,
+	RETURNS,
+	AND,
+	OR,
+	FOR,
+	IN,
+	SWITCH,
+	CASE,
+	BREAK,
+	DEFAULT,
+	WHILE,
+	PLUS,
+	MINUS,
+	MUL,
+	DIV,
+	LT,
+	LE,
+	GE,
+	GT,
+	EQ,
+	NE,
+	DEF,
+	ENDDEF,
+	COLON,
+	RANGEOP,
+	SEMICOL,
+	COMMA,
+	ASSIGNOP,
+	SQBO,
+	SQBC,
+	BO,
+	BC,
+	NUM,
+	RNUM,
+	ID,
+	DRIVERDEF,
+	DRIVERENDDEF,
+	DOLLAR
+} terminal;
+
+/* enum for nonterminals (turned to lower case to remove the conflict with Keywords) */
+typedef enum {
+	program,
+	moduledeclarations,
+	moduledeclaration,
+	othermodules,
+	drivermodule,
+	module,
+	ret,
+	input_plist,
+	input_plistnew,
+	output_plist,
+	output_plistnew,
+	type,
+	datatype,
+	moduledef,
+	statements,
+	statement,
+	iostmt,
+	whichid,
+	_index, /* For removing conflict with index */
+	simplestmt,
+	assignmentstmt,
+	modulereusestmt,
+	optional,
+	idlist,
+	idlistnew,
+	expression,
+	arithorboolexpr,
+	arithorboolexprnew,
+	relopexpr,
+	relopexprnew,
+	arithmeticexpr,	
+	arithmeticexprnew,
+	term,
+	termnew,
+	factor,
+	var,
+	pm,
+	md,
+	logicalop,
+	relationalop,
+	declarestmt,
+	condionalstmt,
+	casestmts,
+	casestmtsnew,
+	value,
+	_default, /* for removing the conflict */
+	iterativestmt,
+	range,
+} nonterminal;
+
+terminal * delimit[];
+
+typedef enum {T, NT} typeOfSymbol;
+
+/* Enumerated interger value of the corresponding T/NT */
+typedef union {
+	terminal T;
+	nonterminal NT;
+} symbol;
+
+typedef struct {
+	symbol sym;
+	typeOfSymbol tag;
+} stackElement;
 #endif
